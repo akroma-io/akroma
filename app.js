779f8c2e4792178934cd2034fb7f99f5ec493d7d
Leonardo Bell <lmbell4@gmail.com>
2016-10-01 19:06:42 -0700

    Adding SASS support and cleaning up express code to use ES6

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
+const pkg = require('./package.json');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const sass = require('node-sass-middleware');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,41 +20,38 @@ app.use(bodyParser.urlencoded({
    extended: false
 }));
 app.use(cookieParser());
+app.use(sass({
+   src: path.join(__dirname, 'modules'),
+   dest: path.join(__dirname, `public/${pkg.name}`),
+   outputStyle: 'compressed',
+   prefix: `/${pkg.name}`
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configure routing
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-   var err = new Error('Not Found');
+app.use((req, res, next) => {
+   const err = new Error('Not Found');
    err.status = 404;
    next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-   app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-         message: err.message,
-         error: err
-      });
-   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+// Handles all errors that slip through
+app.use((err, req, res, next) => {
    res.status(err.status || 500);
    res.render('error', {
       message: err.message,
-      error: {}
+      error: app.get('env') === 'development' ? err : {}
    });
 });
 
+app.locals = {
+   app: {
+      name: pkg.name,
+      version: pkg.version
+   }
+};
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
    res.render('index', {
-      title: 'Express'
+      title: 'Akroma2'
    });
 });
 
